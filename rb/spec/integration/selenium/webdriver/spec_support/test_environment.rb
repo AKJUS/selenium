8fdbf1bbfe77dc5e09ccae5f7e7433f4bd7d6249
Titus Fortner <titus.fortner@gmail.com>
2026-05-23 16:25:14 -0500

    [rb] fix firefox test failure - selenium manager doesn't clear browser version when binary specified

--- rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
@@ -321,7 +321,7 @@ module Selenium
         end
 
         def firefox_options(args: [], **opts)
-          opts[:browser_version] = browser_version
+          opts[:browser_version] = browser_version unless ENV.key?('FIREFOX_BINARY')
           opts[:web_socket_url] = true if ENV['WEBDRIVER_BIDI'] && !opts.key?(:web_socket_url)
           opts[:binary] ||= rlocation(ENV['FIREFOX_BINARY']) if ENV.key?('FIREFOX_BINARY')
           opts[:unhandled_prompt_behavior] ||= 'ignore'
